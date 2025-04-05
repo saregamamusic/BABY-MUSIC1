4de94303d0d7a26d7724f0405c770bc1276f94a1
saregamamusic <bittulkr628@gmail.com>
2025-04-05 22:14:11 +0530

    Update queue.py

--- SONALI/utils/inline/queue.py
+++ SONALI/utils/inline/queue.py
@@ -66,17 +66,9 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
             InlineKeyboardButton(
-                text="𝗔ʙᴏᴜᴛ 𝗔ʟᴘʜᴀ 🥀", url=f"https://t.me/WORLD_ALPHA"
-            ),
-            InlineKeyboardButton(
-                text="𝗦ᴜᴘᴘᴏʀᴛ 𝖦ʀᴏᴜᴘ 🥀", url=f"https://t.me/PURVI_UPDATES"
+                    text=_["CLOSE_BUTTON"]
+                    callback_data="close",
             ),
         ],
       ]
